a3fe6454f2f5487aa24b2bf3fc902acde7853230
Vikram Somu <vikster93@gmail.com>
2014-08-16 04:02:56 -0400

    Name filter

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -18,6 +18,13 @@ angular.module('F1FeederApp.controllers', [])
     ergastAPIservice.getDrivers().success(function (response) {
         //Dig into the responde to get the relevant data
         $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    
+        //Filtering using nameFilter searches all attributes of a model by default
+        //With this, we limit search attributes to only givenName and familyName
+        $scope.searchFilter = function (driver) {
+            var keyword = new RegExp($scope.nameFilter, 'i');
+            return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+        };
     });
     
     // $scope.driversList = [
