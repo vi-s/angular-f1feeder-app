2b3a9f23b73d7d4e382f3310830b47dc37130cf8
Vikram Somu <vikster93@gmail.com>
2014-08-16 02:45:29 -0400

    add driver controller

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,15 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+angular.module('F1FeederApp', [
+  'F1FeederApp.controllers',
+  'F1FeederApp.services'
+]);
--- app/js/services.js
+++ app/js/services.js
@@ -7,3 +7,17 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1');
+
+angular.module('F1FeederApp.services', [])
+  .factory('ergastAPIservice', function($http) {
+    var ergastAPI = {};
+
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    return ergastAPI;
+  });
